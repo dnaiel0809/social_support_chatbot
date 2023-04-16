9814cb139a03d926b944c776741de4b034731e8f
daniel0809 <daniel08099@gmail.com>
2023-04-16 17:26:20 +0800

    task1

--- Task_2.js
+++ Task_2.js
@@ -1,13 +1,15 @@
 var id = localStorage.getItem("ID");
 
 // const scriptURL='https://script.google.com/macros/s/AKfycbzsPOC9Q0_kxMQkP8ZJo2c8lSCxQN1BiLFeC9aVMCUTpPhCV4baXCkwuU7cpLMiGnQT/exec'
-const form = document.forms['form']
+// const form = document.forms['form']
 
 
 form.addEventListener('submit', e => {
+    console.log("button")
     var formdata = new FormData(form)
     formdata.append('ID', id)
     formdata.append('Q', 'C2')
+    console.log("formdata")
     e.preventDefault()
     fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
         .then(response => {alert("You have successfully submitted.");
