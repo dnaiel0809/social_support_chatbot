6728c71ca3a8b892c93accd30e3ab11f73bd180d
daniel0809 <daniel08099@gmail.com>
2023-04-16 20:17:12 +0800

    task1

--- Task_2.js
+++ Task_2.js
@@ -1,29 +1,28 @@
-// window.addEventListener("DOMContentLoaded", (event) => {
-    var id = localStorage.getItem("ID");
-    console.log("task2")
-    const scriptURL='https://script.google.com/macros/s/AKfycbzsPOC9Q0_kxMQkP8ZJo2c8lSCxQN1BiLFeC9aVMCUTpPhCV4baXCkwuU7cpLMiGnQT/exec'
-    const form_2 = document.forms['form_2']
-    console.log("form_2:"+form_2)
-    
-    form_2.addEventListener('submit', e => {
-        console.log("button")
-        var formdata = new FormData(form_2)
-        formdata.append('ID', id)
-        formdata.append('Q', 'C2')
-        console.log("formdata")
-        e.preventDefault()
-        fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
-            .then(response => {alert("You have successfully submitted.");
-                // window.location.href = "Task_3.html";
-        $(function(){
-            $("#includedContent").load("Task_3.html"); 
-        });
-        $.getScript("Task_3.js");
-        })
-        .catch(error => console.error('Error!', error.message))
+var id = localStorage.getItem("ID");
+console.log("task2")
+const scriptURL='https://script.google.com/macros/s/AKfycbzsPOC9Q0_kxMQkP8ZJo2c8lSCxQN1BiLFeC9aVMCUTpPhCV4baXCkwuU7cpLMiGnQT/exec'
+const form_2 = document.forms['form_2']
+console.log("form_2:"+form_2)
+
+form_2.addEventListener('submit', e => {
+    console.log("button")
+    var formdata = new FormData(form_2)
+    formdata.append('ID', id)
+    formdata.append('Q', 'C2')
+    console.log("formdata")
+    e.preventDefault()
+    fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
+        .then(response => {alert("You have successfully submitted.");
+            // window.location.href = "Task_3.html";
+    $(function(){
+        $("#includedContent").load("Task_3.html"); 
+    });
+    $.getScript("Task_3.js");
     })
-    
-// });
+    .catch(error => console.error('Error!', error.message))
+})
+
+
 
 // $(document).on('click', '#submit_2', function() {
 //     $('#submit_2').click(function() {
