d2ad31dfff6820d6989eca9d52de6b246f3008e9
daniel0809 <daniel08099@gmail.com>
2023-04-16 17:47:05 +0800

    task1

--- Task_2.js
+++ Task_2.js
@@ -1,23 +1,26 @@
-var id = localStorage.getItem("ID");
+window.addEventListener("DOMContentLoaded", (event) => {
+    var id = localStorage.getItem("ID");
 
-// const scriptURL='https://script.google.com/macros/s/AKfycbzsPOC9Q0_kxMQkP8ZJo2c8lSCxQN1BiLFeC9aVMCUTpPhCV4baXCkwuU7cpLMiGnQT/exec'
-const form_2 = document.forms['form_2']
+    // const scriptURL='https://script.google.com/macros/s/AKfycbzsPOC9Q0_kxMQkP8ZJo2c8lSCxQN1BiLFeC9aVMCUTpPhCV4baXCkwuU7cpLMiGnQT/exec'
+    const form_2 = document.forms['form_2']
+    console.log("form_2:"+form_2)
 
-
-form_2.addEventListener('submit_2', e => {
-    console.log("button")
-    var formdata = new FormData(form)
-    formdata.append('ID', id)
-    formdata.append('Q', 'C2')
-    console.log("formdata")
-    e.preventDefault()
-    fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
-        .then(response => {alert("You have successfully submitted.");
-            // window.location.href = "Task_3.html";
-    $(function(){
-        $("#body").load("Task_3.html"); 
-    });
-    $.getScript("Task_3.js");
+    form_2.addEventListener('submit_2', e => {
+        console.log("button")
+        var formdata = new FormData(form)
+        formdata.append('ID', id)
+        formdata.append('Q', 'C2')
+        console.log("formdata")
+        e.preventDefault()
+        fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
+            .then(response => {alert("You have successfully submitted.");
+                // window.location.href = "Task_3.html";
+        $(function(){
+            $("#body").load("Task_3.html"); 
+        });
+        $.getScript("Task_3.js");
+        })
+        .catch(error => console.error('Error!', error.message))
     })
-    .catch(error => console.error('Error!', error.message))
-})
+    
+});
