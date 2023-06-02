396c89fd6c28793b7fc94871d9bafbbfee48ca8c
daniel0809 <31154340+dnaiel0809@users.noreply.github.com>
2023-06-02 23:11:43 +0800

    Update Task_3.js

--- Task_3.js
+++ Task_3.js
@@ -1,6 +1,7 @@
 var id = localStorage.getItem("ID");
 console.log("task3")
-// const scriptURL='https://script.google.com/macros/s/AKfycbzsPOC9Q0_kxMQkP8ZJo2c8lSCxQN1BiLFeC9aVMCUTpPhCV4baXCkwuU7cpLMiGnQT/exec'
+var next = localStorage.getItem("order3")
+const scriptURL='https://script.google.com/macros/s/AKfycbwFEjvw0g-D76hXmh3FqvkhpPWM65RaSqUNeGEv6WLYenfydBd9HrZM7RsseKVDZRwB/exec'
 var form_3 = document.forms['form_3']
 console.log("form_3:"+form_3)
 
@@ -14,8 +15,8 @@ form_3.addEventListener('submit', e => {
     fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
         .then(response => {alert("You have successfully submitted. By clicking the button \"I\'ve submitted task,\" let the chatbot know you are moving to the next task.");
     $(function(){
-        $("#includedContent").load("Task_4.html",function () {
-          $.getScript("Task_4.js");
+        $("#includedContent").load(next+".html",function () {
+          $.getScript(next+".js");
         }); 
     });
     })
