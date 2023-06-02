ed91c882362d7b9e55a9b3d6af828db84ccb0b3c
daniel0809 <31154340+dnaiel0809@users.noreply.github.com>
2023-06-03 01:37:03 +0800

    Update Task_3.js

--- Task_3.js
+++ Task_3.js
@@ -1,6 +1,6 @@
 var id = localStorage.getItem("ID");
 console.log("task3")
-var next = localStorage.getItem("order3")
+var next = localStorage.getItem("order4")
 var scriptURL='https://script.google.com/macros/s/AKfycbwFEjvw0g-D76hXmh3FqvkhpPWM65RaSqUNeGEv6WLYenfydBd9HrZM7RsseKVDZRwB/exec'
 var form_3 = document.forms['form_3']
 console.log("form_3:"+form_3)
