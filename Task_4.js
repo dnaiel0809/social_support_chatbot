00d49546c595ea396bed12acc5de10da2c88cfd9
daniel0809 <31154340+dnaiel0809@users.noreply.github.com>
2023-06-03 12:10:37 +0800

    Update Task_4.js

--- Task_4.js
+++ Task_4.js
@@ -1,7 +1,7 @@
 var id = localStorage.getItem("ID");
 var index = localStorage.getItem("index")
 var next = localStorage.getItem("order"+index)
-localStorage.setItem("index", index+1);
+localStorage.setItem("index", String(Number(index)+1));
 
 var scriptURL='https://script.google.com/macros/s/AKfycbwFEjvw0g-D76hXmh3FqvkhpPWM65RaSqUNeGEv6WLYenfydBd9HrZM7RsseKVDZRwB/exec'
 var form_4 = document.forms['form_4']
