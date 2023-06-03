21147bb740493e4b990b382d4e409d123622093a
daniel0809 <31154340+dnaiel0809@users.noreply.github.com>
2023-06-03 13:09:59 +0800

    Update Task_1.js

--- Task_1.js
+++ Task_1.js
@@ -13,17 +13,21 @@ formdata.append('ID', id)
 formdata.append('Q', 'C1')
 
 e.preventDefault()
-  fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
-            .then(response => {alert(next=='Post-survey'?"You have successfully submitted. Well done.":"You have successfully submitted. By clicking the button \"I\'ve submitted task,\" let the chatbot know you are moving to the next task.");
-// 					 for (var pair of formdata.entries()) {
-//  					   console.log(pair[0]+ ', ' + pair[1]); 
-// 					}
-    // window.location.href = "Task_2.html";
+  if (next=='Post-survey'){
+        fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
+    .then(response => {alert("You have successfully submitted. Well done.");
+          window.location.href = "Post-survey.html";
+          })
+    .catch(error => console.error('Error!', error.message))
+    }else{
+
+    fetch(scriptURL, { mode: "no-cors",method: 'POST', body: formdata})
+        .then(response => {alert("You have successfully submitted. By clicking the button \"I\'ve submitted task,\" let the chatbot know you are moving to the next task.");
     $(function(){
         $("#includedContent").load(next+".html",function () {
 //           $.getScript(next+".js");
         }); 
     });
     })
-    .catch(error => console.error('Error!', error.message))
+    }   
     })
